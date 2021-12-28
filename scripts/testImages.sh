dd3872d14bba2cac7b0e7420838935c1e8c59e51
Hugo Moinheiro <hugo.moinheiro@ua.pt>
2021-12-28 18:37:20 +0000

    script update

--- scripts/testImages.sh
+++ scripts/testImages.sh
@@ -4,16 +4,20 @@ cd ../
 
 make
 
-#images="ls ./Images/*.jpg"
 cd Images
 images=$(ls *.jpg)
 cd ..
 for imageIn in $images
 do
-    imageOut="./Images_Out/$imageIn"
-    echo "./Images/"$imageIn
-    echo $imageOut 
-    ./opencvTest "./Images/"$imageIn $imageOut
+    for i in {0..2}
+    do
+        imageOut="./Images_Out/$imageIn"
+        imageCoded="${imageOut}Coded${i}"
+        echo "./Images/"$imageIn
+        echo $imageOut 
+        echo $imageCoded
+        ./opencvTest "./Images/"$imageIn $imageOut $imageCoded $i
+    done
 done
 
 cd Images
@@ -21,10 +25,34 @@ images=$(ls *.png)
 cd ..
 for imageIn in $images
 do
-    imageOut="./Images_Out/$imageIn"
-    echo "./Images/"$imageIn
-    echo $imageOut 
-    ./opencvTest "./Images/"$imageIn $imageOut
+    for i in {0..2}
+    do
+        imageOut="./Images_Out/$imageIn"
+        imageCoded="${imageOut}Coded${i}"
+        echo "./Images/"$imageIn
+        echo $imageOut
+        echo $imageCoded
+        ./opencvTest "./Images/"$imageIn $imageOut $imageCoded $i
+    done
+done
+
+
+cd Images_Out
+imagesOut=$(ls *.jpg)
+cd ..
+for image in $images
+do
+    filesize=$(stat -c %s $image)
+    echo "${image}    ${filesize} bytes"
+done
+
+cd Images_Out
+imagesOut=$(ls *.png)
+cd ..
+for image in $images
+do
+    filesize=”$(stat -c %s $image)
+    echo "${image}    ${filesize} bytes"
 done
 
 cd scripts
